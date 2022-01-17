edece471a734986ab01863507b73dbdc02de997d
John Mook <jedwardmook@yahoo.com>
2022-01-17 11:53:15 -0600

    commit tree 7d8f80d20998e099ed42edf65e02d73b9040bc7f

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers,name){
+    return drivers.filter(function(findDriver) {
+        return findDriver.toLowerCase() === name.toLowerCase();
+    })
+};
+
+function fuzzyMatch(drivers,name){
+    const driversLetter = name.length
+    return drivers.filter(function(findDriver){
+        return findDriver.slice(0,driversLetter) === name
+    })
+};
+
+function matchName(drivers, key) {
+    const result = drivers.filter(function(driver) {
+        return driver.name === key})
+    return result;
+};
